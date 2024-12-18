40371f7b8960f8cec3c898624edf887d9ecb87db
Holden Thompson <holdenjthompson@icloud.com>
2024-12-17 20:44:40 -0500

    Started Collection Labs

--- student/2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
+++ student/2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -3,16 +3,19 @@
  
  Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
  */
-
+var registrationList: [String] = []
 
 //:  Your friend Jasmine is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
-
+registrationList.append("Jasmine")
+registrationList.count
+print(registrationList)
 
 //:  Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
+registrationList += ["Amy", "Lou", "Chelsea", "Dan"]
 
 
 //:  Use the `insert(_:at:)` method to add `Trey` into the array as the second element. Print the contents of the collection.
-
+registrationList.insert("Trey", at: 1)
 
 //:  Somebody had a conflict and decided to transfer registration to someone else. Use array subscripting to change the sixth element to `Elton`. Print the contents of the collection.
 
